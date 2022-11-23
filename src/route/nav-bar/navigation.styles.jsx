dfd9cdfba3813b69b3cda1f93a8fbb90df5e9e94
Ha_Kyo <luongthithuyha2612@gmail.com>
2022-11-23 15:53:46 +0700

    change navbar to fixed position

--- src/route/nav-bar/navigation.styles.jsx
+++ src/route/nav-bar/navigation.styles.jsx
@@ -2,8 +2,14 @@ import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 export const NavigationContainer=styled.div`
  height: 70px;
-  width: 100%;
+  width: 95%;
+  position:fixed;
+  font-size: 20px;
+  top:0;
   display: flex;
+  background-color: white;
+  z-index:1;
+  padding: 20px;
   justify-content: space-between;
   margin-bottom: 25px;
 `
@@ -15,9 +21,10 @@ export const LogoContainer=styled.div`
 export const NavLinks=styled.div`
 width: 50%;
 height: 100%;
-display: flex;
+display:flex;
 align-items: center;
-justify-content: flex-end;
+justify-content:flex-end;
+right: 30px;
 `
 export const NavLink=styled(Link)`
 padding: 10px 15px;
